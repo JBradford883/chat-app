d7974c59530503251b5d5a87ffe665e9c6f982c8
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-08-25 16:57:32 -0400

    Import google font Poppins

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+// Change font to Poppins
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_600SemiBold,
+} from '@expo-google-fonts/poppins';
+
 // Import the screens
 import Start from './components/Start';
 import Chat from './components/Chat';
@@ -14,26 +22,31 @@ import { createStackNavigator } from '@react-navigation/stack';
 // Create the navigator
 const Stack = createStackNavigator();
 
-import { StyleSheet, Text, View } from 'react-native';
+export default function App() {
+  let [fontsLoaded] = useFonts({
+    Regular: Poppins_400Regular,
+    Bold: Poppins_600SemiBold
+  });
 
-export default class App extends React.Component {
-
-  render() {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Start"
-        >
-          <Stack.Screen
-            name="Start"
-            component={Start}
-          />
-          <Stack.Screen
-            name="Chat"
-            component={Chat}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
+  if (!fontsLoaded) {
+    return <AppLoading />;
   }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Start"
+      >
+        <Stack.Screen
+          name="Start"
+          component={Start}
+        />
+        <Stack.Screen
+          name="Chat"
+          component={Chat}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
